b00e9f57ef2b840e7a7568c96bc961e16ef6d9af
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 14:45:43 +0530

    update dashboard

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {DashboardHeaderComponent} from "../../shared/dashboard-header/dashboard-header.component";
+import {DashboardNavComponent} from "../../shared/dashboard-nav/dashboard-nav.component";
+import {RouterOutlet} from "@angular/router";
+import {DashboardFooterComponent} from "../../shared/dashboard-footer/dashboard-footer.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    DashboardHeaderComponent,
+    DashboardNavComponent,
+    RouterOutlet,
+    DashboardFooterComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
