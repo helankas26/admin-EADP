de2304f420ee802d16bc5f196565c3731d93e2c6
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 14:38:32 +0530

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard/dashboard.component";
+import {
+  HomeDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/home/home-default/home-default.component";
+import {
+  CartDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/cart/cart-default/cart-default.component";
+import {
+  CustomerDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/customer/customer-default/customer-default.component";
+import {
+  CategoryDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/category/category-default/category-default.component";
+import {
+  DiscountDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/discount/discount-default/discount-default.component";
+import {
+  ProductDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/product/product-default/product-default.component";
+import {
+  ReviewDefaultComponent
+} from "./components/dashboard/dashboard/inner-items/review/review-default/review-default.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+      {path: 'home', component: HomeDefaultComponent},
+      {path: 'cart', component: CartDefaultComponent},
+      {path: 'customer', component: CustomerDefaultComponent},
+      {path: 'category', component: CategoryDefaultComponent},
+      {path: 'discount', component: DiscountDefaultComponent},
+      {path: 'product', component: ProductDefaultComponent},
+      {path: 'review', component: ReviewDefaultComponent},
+    ]
+  }
+];
