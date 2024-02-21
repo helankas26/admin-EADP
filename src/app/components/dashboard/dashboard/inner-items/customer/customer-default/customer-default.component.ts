967290696d00b08672b061c3d6e6d449d9f506bc
Helanka Singhapurage <helankas26@gmail.com>
2024-02-21 17:49:26 +0530

    customer ui

--- src/app/components/dashboard/dashboard/inner-items/customer/customer-default/customer-default.component.ts
+++ src/app/components/dashboard/dashboard/inner-items/customer/customer-default/customer-default.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-customer-default',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './customer-default.component.html',
   styleUrl: './customer-default.component.scss'
 })
