b42865640fac040cdccaaa1265054fd92cd6a1e1
Helanka Singhapurage <helankas26@gmail.com>
2024-02-21 19:26:31 +0530

    add loading and cookie service

---
+++ src/app/components/services/cookie-manager.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookieManagerService {
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  public createToken(value: any) {
+    this.cookieService.set('token', value, 30, '/');
+  }
+
+  public isTokenExists(): boolean {
+    return this.cookieService.check('token');
+  }
+
+  public removeToken() {
+    if (this.isTokenExists()) {
+      this.cookieService.delete('token');
+    }
+  }
+
+  public getToken(): string {
+    /*   if (this.isTokenExists()) {
+         return this.cookieService.get('token');
+       }
+       return '';*/
+    return this.isTokenExists() ? this.cookieService.get('token') : '';
+  }
+}
---
+++ src/app/components/services/loading-status.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingStatusService {
+
+  public loadingState = new BehaviorSubject(true);
+
+  constructor() {
+  }
+}
